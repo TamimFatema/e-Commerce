0d38a52d73332828b47279278cd505dc76c4b0a5
TamimFatema <1910807@iub.edu.bd>
2023-08-13 17:41:02 +0600

    signup login

--- Kinakata/App/models.py
+++ Kinakata/App/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 
 
 class Category (models.Model):
@@ -30,3 +31,26 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email', error_messages={
+                             'exists': 'This User Exists'})
+
+
+class Meta:
+    model = User
+    fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+    def clean_emaill(self):
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise forms.ValidationError(
+                self.fields['email'].error_message['exists'])
+        return self.cleaned_data['email']
--- Kinakata/Kinakata/views.py
+++ Kinakata/Kinakata/views.py
@@ -2,20 +2,29 @@ from itertools import product
 from django.shortcuts import render
 from App.models import Category, Product
 
+from django.contrib.auth import authenticate, login
+from App.models import UserCreateForm
+
 
 def main(request):
+    category = Category.objects.all()
+    context = {
+        'category': category,
+    }
 
-    return render(request, 'main.html')
+    return render(request, 'main.html', context)
 
 
 def index(request):
-    category = Category.objects.all()
 
     product = Product.objects.all()
 
     context = {
-        'category': category,
 
         'product': product,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def signup(request):
+    return render(request, 'registration/signup.html')
